e833482b368939d3187c06bbdec25ef7450ad1b2
AzureAD\JamieFleming <Jamie.Fleming@reg.uk.com>
2025-01-06 19:47:17 +0000

    Update CORS for GitHub Pages

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express();
 
 // Updated CORS configuration
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://eatreal.co.uk'],
-    methods: ['GET', 'POST', 'OPTIONS'],
+    origin: [
+        'http://127.0.0.1:5500',
+        'http://localhost:5500',
+        'https://eatreal.github.io',
+        'https://eatreal.co.uk'
+    ],
+    methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Accept', 'Origin'],
     credentials: false
 }));
