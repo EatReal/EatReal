6b9eea24cef297aef0958bd1b3ef52db9da09270
EatReal <eatreal47@gmail.com>
2025-01-06 15:11:28 +0000

    Delete assets/js/config.js

--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-// Replace with environment variables in production
-const config = {
-    paypalClientId: 'AW3v5ILvYVtVENPuGqdL8rCkNMV_CrAF25zEymHGuAwYXAmFM4ddRGGVnmQzcMa45Bjk4t49NmwAsQLC',
-    mongoDbUri: 'mongodb+srv://eatreal47:sk8VBLVqfjhZQ2hV@cluster0.0fbvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    emailUser: 'eatreal47@gmail.com',
-    emailPassword: 'gwzfaixhhuldnmfw',
-    adminPassword: 'EatReal2024!'
-}; 
